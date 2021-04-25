0058c21f22defa7c1e9d97b2f5728477fa7e90e1
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-04-25 18:56:08 -0400

    25 run

--- slide22.sh
+++ slide22.sh
@@ -5,34 +5,34 @@ python3 Scripts/create_input.py 3 20
 
 ../mp-spdz-0.2.3/Scripts/setup-ssl.sh 20
 
-echo -------------------------------------------------------------------
-echo -------------------------------------------------------------------
-echo -------------------------------------------------------------------
-echo -------------------------------------------------------------------
+# echo -------------------------------------------------------------------
+# echo -------------------------------------------------------------------
+# echo -------------------------------------------------------------------
+# echo -------------------------------------------------------------------
 
-echo average_by_gender_all_secured - semi-honest
-../mp-spdz-0.2.3/compile.py average_by_gender_all_secured.mpc
-../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_all_secured
+# echo average_by_gender_all_secured - semi-honest
+# ../mp-spdz-0.2.3/compile.py average_by_gender_all_secured.mpc
+# ../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_all_secured
+# sleep 150
+# echo -------------------------------------------------------------------
 
-echo -------------------------------------------------------------------
-
-echo average_by_gender_optimized - semi-honest
-../mp-spdz-0.2.3/compile.py average_by_gender_optimized.mpc
-../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_optimized
-
-echo -------------------------------------------------------------------
-echo -------------------------------------------------------------------
+# echo average_by_gender_optimized - semi-honest
+# ../mp-spdz-0.2.3/compile.py average_by_gender_optimized.mpc
+# ../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_optimized
+# sleep 150
+# echo -------------------------------------------------------------------
+# echo -------------------------------------------------------------------
 
 # echo average_by_gender_all_secured - malicious
 # ../mp-spdz-0.2.3/compile.py average_by_gender_all_secured.mpc
 # ../mp-spdz-0.2.3/Scripts/mal-shamir.sh average_by_gender_all_secured
-
+# sleep 150
 # echo -------------------------------------------------------------------
 
 # echo average_by_gender_optimized  - malicious
 # ../mp-spdz-0.2.3/compile.py average_by_gender_optimized.mpc
 # ../mp-spdz-0.2.3/Scripts/mal-shamir.sh average_by_gender_optimized
-
+# sleep 150
 echo -------------------------------------------------------------------
 echo -------------------------------------------------------------------
 echo -------------------------------------------------------------------
@@ -47,20 +47,20 @@ echo -------------------------------------------------------------------
 echo average_by_gender_preprocessing_all_secured - semi-honest
 ../mp-spdz-0.2.3/compile.py average_by_gender_preprocessing_all_secured.mpc
 ../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_preprocessing_all_secured
-
+sleep 150
 echo -------------------------------------------------------------------
 
 echo average_by_gender_preprocessing_optimized - semi-honest
 ../mp-spdz-0.2.3/compile.py average_by_gender_preprocessing_optimized.mpc
 ../mp-spdz-0.2.3/Scripts/shamir.sh average_by_gender_preprocessing_optimized
-
+sleep 150
 echo -------------------------------------------------------------------
 echo -------------------------------------------------------------------
 
 echo average_by_gender_preprocessing_all_secured - malicious
 ../mp-spdz-0.2.3/compile.py average_by_gender_preprocessing_all_secured.mpc
 ../mp-spdz-0.2.3/Scripts/mal-shamir.sh average_by_gender_preprocessing_all_secured
-
+sleep 150
 echo -------------------------------------------------------------------
 
 echo average_by_gender_preprocessing_optimized  - malicious
